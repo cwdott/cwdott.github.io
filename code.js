66ae047a1b48c517e7fce9c9102d06913da675d7
cwdott <phil@capitalwebdesign.ca>
2018-02-23 14:00:16 -0500

    Changed elem

--- code.js
+++ code.js
@@ -2,9 +2,11 @@ const $ = (selector) => document.querySelector(selector)
 const $$ = (selector) => document.querySelectorAll(selector)
 const on = (elem, type, listener) => elem.addEventListener(type,listener)
 
-on($('#toggle-left'),'click',()=>{
+on($("button[id='toggle-left']"),'click',()=>{
    $$(".start").forEach((elem) => elem.classList.toggle('closed'))
 })
-on($('#toggle-right'),'click',()=>{
+on($("button[id='toggle-right']"),'click',()=>{
    $$(".end").forEach((elem) => elem.classList.toggle('closed'))
 })
+
+

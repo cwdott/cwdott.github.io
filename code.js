07e98b2f6d82a8cb20afcf50477afe39d4ccee3e
cwdott <phil@capitalwebdesign.ca>
2018-02-23 14:34:59 -0500

    code

--- code.js
+++ code.js
@@ -2,8 +2,8 @@ var toggleLeft = document.querySelector('button[id="toggle-left"]');
 var toggleRight = document.querySelector('button[id="toggle-right"]');
 
 function toggleClose(elem) {
-    elem.classList.toggle("closed");
+    document.querySelector(elem).classList.toggle("closed");
 }
 
-toggleLeft.addEventListener('click',function() { toggleClose(start); },false);
-toggleRight.addEventListener('click',function() { toggleClose(end); },false);
+toggleLeft.addEventListener('click',function() { toggleClose("start"); },false);
+toggleRight.addEventListener('click',function() { toggleClose("end"); },false);
